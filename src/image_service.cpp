fa323d513f84cbd833fefcb01a3c7e8263db9e2b
lanzheng.liulz <lanzheng.liulz@alibaba-inc.com>
2021-07-19 18:58:17 +0800

    support subrepo

--- src/image_service.cpp
+++ src/image_service.cpp
@@ -112,7 +112,10 @@ int parse_blob_url(const std::string &url, struct ImageRef &ref) {
             if (words.size() != 5) {
                 LOG_ERROR_RETURN(0, -1, "invalid blob url: `", url);
             }
-            ref.seg = std::vector<std::string>{words[0], words[2], words[3]};
+            ref.seg = std::vector<std::string>{words[0]};
+            for (int i = 2; i + 1 < words.size(); i++) {
+                ref.seg.push_back(words[i]);
+            }
         }
     }
     return 0;
